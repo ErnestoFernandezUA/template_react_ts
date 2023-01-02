0dc7f83f93d9d66e451e5a4eb129903fb769f4ef
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-01-02 07:10:33 +0200

    add toolkit-persist-axios-api-utils-type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { getPostsAsync, selectPosts } from './features/Posts/postsSlice';
+import './App.scss';
 
 function App() {
+  const dispatch = useAppDispatch();
+  const posts = useAppSelector(selectPosts);
+
+  // eslint-disable-next-line no-console
+  console.log(posts);
+
+  useEffect(() => {
+    dispatch(getPostsAsync());
+  }, [dispatch])
+
   return (
     <div className="App">
-      React Template
-    </div>
+        React Template
+      </div>
   );
 }
 
